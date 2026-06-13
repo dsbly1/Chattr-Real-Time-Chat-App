acec0939cc3c1f7b1201dfca87b33338926cf5f3
dsbly1 <dsbly1@cougars.ccis.edu>
2026-06-13 01:32:06 -0500

    feat: create Express server with Socket.io attached

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+require('./socket')(io);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`🚀 Chattr running at http://localhost:${PORT}`);
+});
